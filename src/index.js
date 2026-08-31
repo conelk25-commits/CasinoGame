cc6f768132c51921717502ee7220a05e825aabbc
conelk25-commits <conelk25@gmail.com>
2026-08-31 14:42:00 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CasinoGame from '../CasinoGame';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CasinoGame />
+  </React.StrictMode>
+);
